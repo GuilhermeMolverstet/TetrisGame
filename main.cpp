290f0930904b322fee37d7a55e18b981175bcf5c
Guilherme Molverstet <guilhermemolver12@gmail.com>
2025-10-01 14:06:49 -0300

    ComentariosMain

--- main.cpp
+++ main.cpp
@@ -40,16 +40,16 @@ int main ()
         DrawTextEx(font, "Next", {370, 175}, 38, 2, WHITE);
         if (game.gameOver)
         {        
-            DrawTextEx(font, "GAME OVER", {320, 450}, 38, 2, WHITE);
+            DrawTextEx(font, "GAME OVER", {320, 450}, 38, 2, WHITE); // Se o jogo acabou, mostra essa mensagem
         }
         DrawRectangleRounded({320,55,170,60}, 0.3, 6, azulClaro);
 
-        char scoreText[10];
+        char scoreText[10]; // Faz o converte da pontuação em texto para ser exibido
         sprintf(scoreText, "%d", game.score);
         Vector2 textSize = MeasureTextEx(font, scoreText, 38, 2);
 
         DrawTextEx(font, scoreText, {320 + (170 - textSize.x) / 2, 65}, 38, 2, WHITE);
-        DrawRectangleRounded({320,215,170,180}, 0.3, 6, azulClaro);
+        DrawRectangleRounded({320,215,170,180}, 0.3, 6, azulClaro); // Caixa para mostrar a proxima peça
         game.Draw(); // Desenho do tabuleiro e peças
         EndDrawing();
     }
